1c6329debebd7c0c4943c45f2d4e3561b03fc75e
Eirik Furuseth <alpinelinux@heplaphon.com>
2024-06-22 23:41:36 +0200

    feat: update install.sh to find own filepath

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
  #!/bin/bash
 #Variables
 #dotfiles_dir = pwd
-
+#
+BASEDIR=$(dirname $0)
+SCRIPT_DIR=$( cd -- "$( dirname -- '${BASH_SOURCE[0]}' )" &> /dev/null && pwd )
 #Delete
-rm -rf ~/.vimrc > /dev/null 2>&1
-
+#rm -rf ~/.vimrc > /dev/null 2>&1
 #Make Symlink
-ln -sf ~/.dotfiles/.vimrc   ~/.vimrc
+echo "$SCRIPT_DIR"
+ln -sf "$SCRIPT_DIR/.vimrc" ~/.vimrc
